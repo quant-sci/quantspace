64e8ee97f3f16378c1ac0aeb393c9ef99af5b1b7
Kleyt0n <kleyton.vsc@gmail.com>
2024-01-01 23:14:47 -0300

    add: new metrics and update readme

--- quantspace/risk/_metrics.py
+++ quantspace/risk/_metrics.py
@@ -18,3 +18,77 @@ def sharpe_ratio(returns, risk_free_rate):
     sharpe_ratio = excess_return / std_deviation
     
     return sharpe_ratio
+
+def sortino_ratio(returns, risk_free_rate):
+    """
+    Calculate the Sortino ratio of an investment.
+
+    Parameters:
+    returns (array-like): An array or list of investment returns.
+    risk_free_rate (float): The risk-free rate of return.
+
+    Returns:
+    float: The Sortino ratio of the investment.
+    """
+    mean_return = np.mean(returns)
+    downside_returns = [ret - risk_free_rate for ret in returns if ret < risk_free_rate]
+    
+    downside_std_dev = np.std(downside_returns)
+    
+    sortino_ratio = (mean_return - risk_free_rate) / downside_std_dev if downside_std_dev != 0 else np.nan
+    
+    return sortino_ratio
+
+
+def value_at_risk(returns, alpha=0.95):
+    """
+    Calculate the value at risk (VaR) of an investment.
+
+    Parameters:
+
+    returns (array-like): An array or list of investment returns.
+    alpha (float): The confidence level.
+
+    Returns:
+    float: The VaR of the investment.
+    """
+    var = -np.percentile(returns, alpha * 100)
+    return var
+
+
+def conditional_value_at_risk(returns, alpha=0.95):
+    """
+    Calculate the conditional value at risk (CVaR) of an investment.
+
+    Parameters:
+    returns (array-like): An array or list of investment returns.
+    alpha (float): The confidence level.
+
+    Returns:
+    float: The CVaR of the investment.
+    """
+    var = value_at_risk(returns, alpha)
+    cvar = -np.mean([ret for ret in returns if ret < -var])
+    return cvar
+
+def beta(asset_returns, market_returns=None):
+    """
+    Calculate the beta of an investment.
+
+    Parameters:
+    asset_returns (array-like): An array or list of asset returns.
+    market_returns (array-like): An array or list of market returns.
+
+    Returns:
+    float: The beta of the investment.
+    """
+
+    if market_returns is None:
+        market_returns = asset_returns
+        print("Info (beta metric): Market returns not provided. Using asset returns as market returns.")
+    covar = np.cov(asset_returns, market_returns)[0, 1]
+    market_variance = np.var(market_returns)
+    
+    beta = covar / market_variance if market_variance != 0 else np.nan
+    
+    return beta
--- quantspace/risk/_tables.py
+++ quantspace/risk/_tables.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sys
 sys.path.append("./")
-from quantspace.risk import sharpe_ratio
+from quantspace.risk import sharpe_ratio, sortino_ratio, value_at_risk, conditional_value_at_risk, beta
 
-def summary_risk(returns, risk_free_rate):
+def summary_risk(returns, market_returns=None, risk_free_rate=0.05, alpha=0.95):
     """
     Calculate the Sharpe ratio of an investment.
 
@@ -16,12 +16,16 @@ def summary_risk(returns, risk_free_rate):
     """
     METRICS = {
         'Sharpe Ratio': sharpe_ratio(returns, risk_free_rate),
+        'Sortino Ratio': sortino_ratio(returns, risk_free_rate),
+        'Value at Risk': value_at_risk(returns, alpha),
+        'Conditional Value at Risk': conditional_value_at_risk(returns, alpha),
+        'Beta': beta(returns, market_returns),
     }
     table = pd.DataFrame()
     for metric in METRICS:
         table[metric] = [METRICS[metric]]
 
-    return table
+    return table.T.rename(columns={0: 'Risk Metrics'})
     
     
 
